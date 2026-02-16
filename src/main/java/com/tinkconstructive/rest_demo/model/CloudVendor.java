e14f59fea36b213af1081dc3535ad141c4c57d13
DineshHewage <hewage.d.sampath@gmail.com>
2026-02-16 12:49:13 +0530

    Latest changers

--- src/main/java/com/tinkconstructive/rest_demo/model/CloudVendor.java
+++ src/main/java/com/tinkconstructive/rest_demo/model/CloudVendor.java
@@ -18,49 +18,4 @@ public class CloudVendor {
     private String vendorName;
     private String vendorAddress;
     private String vendorPhoneNumber;
-<<<<<<< HEAD
-=======
-
-    public CloudVendor(){
-    }
-
-    public CloudVendor(String vendorId, String vendorName, String vendorAddress, String vendorPhoneNumber) {
-        this.vendorId = vendorId;
-        this.vendorName = vendorName;
-        this.vendorAddress = vendorAddress;
-        this.vendorPhoneNumber = vendorPhoneNumber;
-    }
-
-    public String getVendorAddress() {
-        return vendorAddress;
-    }
-
-    public void setVendorAddress(String vendorAddress) {
-        this.vendorAddress = vendorAddress;
-    }
-
-    public String getVendorId() {
-        return vendorId;
-    }
-
-    public void setVendorId(String vendorId) {
-        this.vendorId = vendorId;
-    }
-
-    public String getVendorName() {
-        return vendorName;
-    }
-
-    public void setVendorName(String vendorName) {
-        this.vendorName = vendorName;
-    }
-
-    public String getVendorPhoneNumber() {
-        return vendorPhoneNumber;
-    }
-
-    public void setVendorPhoneNumber(String vendorPhoneNumber) {
-        this.vendorPhoneNumber = vendorPhoneNumber;
-    }
->>>>>>> 355e289a52e4b80d37bfcdb08616f38a75ba5b4e
 }
